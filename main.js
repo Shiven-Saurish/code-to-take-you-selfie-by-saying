f1e9450b832d64808c5f9ad155f6f40884643eec
Shiven-Saurish <113611504+Shiven-Saurish@users.noreply.github.com>
2023-03-03 19:22:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+ recognition.onresult = function(event) {
+
+    console.log(event);
+
+var Content = event.results[0][0].transcript;
+
+document.getElementById("textbox").innerHTML = Content;
+console.log(Content);
+if(Content == "take my selfie")
+{
+    console.log("taking selfie ---");
+    speak();
+}
+}
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking your selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save()
+    }, 5000);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:180
+});
+camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+        });
+}
